0cf2b4887b8992dd930aec438cbf637d48c73786
Nancy Jain <nancyjain0631@gmail.com>
2021-04-09 21:58:07 +0530

    5

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './Sidebar.css';
+import {Avatar} from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from  "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 function Sidebar() {
     return (
         <div className="sidebar">
            <div className="sidebar_header">
+                <Avatar/>
+                <div className="sidebar_headerRight">
+                    <DonutLargeIcon/>
+                    <ChatIcon/>
+                    <MoreVertIcon/>
+                </div>
 
            </div>
             <div className="sidebar_search">
