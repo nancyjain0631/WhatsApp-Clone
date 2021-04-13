d0f7c91b1c9f88fd3be19e72c8fd7289372397dc
Nancy Jain <nancyjain0631@gmail.com>
2021-04-13 22:26:06 +0530

    3

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reducer, {initialState} from "./reducer";
 // import reportWebVitals from './reportWebVitals';
-
+import { StateProvider } from "./StateProvider";
 ReactDOM.render(
   <React.StrictMode>
     <StateProvider initialState={initialState} reducer={reducer}>
